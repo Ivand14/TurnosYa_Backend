14791ce0298efbabfeef8b9d44e490bd02baafb7
Ivand14 <carpivan14@hotmail.com>
2025-06-19 10:11:55 -0300

    Add salesman_data route to retrieve seller information from Mercado Pago

---
+++ routes/mercado_pago/salesman_data.py
@@ -0,0 +1,41 @@
+from flask import jsonify,Blueprint
+from config.firebase_service import db
+import requests
+
+SALESMAN_DATA = Blueprint("SALESMAN_DATA",__name__)
+
+@SALESMAN_DATA.route("/salesman/<businessId>",methods=["GET"])
+def salesman_data(businessId):
+    mp_data = db.collection("empresas").where("id","==",businessId).stream()
+    
+    access_token = None
+    
+    for doc in mp_data:
+        business = doc.to_dict()
+        if "mercado_pago" not in business:
+            jsonify({
+                "status":400,
+                "message":"Esta cuenta no esta vinculada a mercado pago"
+            })
+        access_token = doc["mercado_pago"]["access_token"]
+    
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+    url = "https://api.mercadopago.com/users/me"
+
+    
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        return jsonify({
+            "status": 500,
+            "message": "Error al consultar los datos del vendedor",
+            "error": str(e)
+        })
+
+    return jsonify({
+        "status": 200,
+        "details": response.json()
+    })

dbaf74a628c118419737365d99268dd464c58adb
Ivand14 <carpivan14@hotmail.com>
2025-06-19 20:59:05 -0300

    Refactor CORS and socket.io initialization for improved readability and maintainability

--- app.py
+++ app.py
@@ -40,10 +40,20 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.environ.get("SUPER_SECRET_KEY")
 
-socketio.init_app(app,cors_allowed_origins=["http://localhost:8080","https://turno-ya-ivand14s-projects.vercel.app","https://turno-ya.vercel.app/"])  
+socketio.init_app(app, cors_allowed_origins=[
+    "http://localhost:8080",                  # Para tu desarrollo local
+    "http://127.0.0.1:8080",                  # Si tu frontend en local usa esta IP
+    "https://turno-ya-ivand14s-projects.vercel.app", # Tu frontend en Vercel (proyecto)
+    "https://turno-ya.vercel.app/"            # Tu frontend en Vercel (dominio principal)
+])
 
 
-CORS(app, origins=["http://localhost:8080","https://turno-ya-ivand14s-projects.vercel.app","https://turno-ya.vercel.app/"])
+CORS(app, origins=[
+    "http://localhost:8080",                  # Para tu desarrollo local
+    "http://127.0.0.1:8080",                  # Si tu frontend en local usa esta IP
+    "https://turno-ya-ivand14s-projects.vercel.app", # Tu frontend en Vercel (proyecto)
+    "https://turno-ya.vercel.app/"            # Tu frontend en Vercel (dominio principal)
+])
 
 
 app.register_blueprint(SIGNUP_BP)

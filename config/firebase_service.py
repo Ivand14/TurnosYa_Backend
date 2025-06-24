6743ca45c27e3df6f1e189a533f61319d27f1b7c
Ivand14 <carpivan14@hotmail.com>
2025-06-24 09:03:52 -0300

    Refactor Firebase initialization to improve structure and error handling

--- config/firebase_service.py
+++ config/firebase_service.py
@@ -1,22 +1,26 @@
 import firebase_admin
-from firebase_admin import credentials,firestore
-
+from firebase_admin import credentials, firestore
 import json
+import threading
 
-secret_path = "/etc/secrets/turnos_ya.json"
-
-with open(secret_path) as f:
-    firebase_config = json.load(f)
+db = None  
 
 def initialize_firebase():
-    cred = credentials.Certificate(
-            firebase_config
-    )
-    firebase_admin.initialize_app(cred,{
-        "storageBucket": "turnosya-c5672.firebasestorage.app"
-    })
-    print("Firebase SDK inicializado correctamente.")
-    return firestore.client()
+    global db
+    try:
+        secret_path = "/etc/secrets/turnos_ya.json"
+        with open(secret_path) as f:
+            firebase_config = json.load(f)
 
-db = initialize_firebase()
-    
+        cred = credentials.Certificate(firebase_config)
+        firebase_admin.initialize_app(cred, {
+            "storageBucket": "turnosya-c5672.firebasestorage.app"
+        })
+        db = firestore.client()
+        print("✅ Firebase SDK inicializado correctamente.")
+    except Exception as e:
+        print("❌ Error al inicializar Firebase:", e)
+
+
+firebase_thread = threading.Thread(target=initialize_firebase)
+firebase_thread.start()

cf90e08d74f840b89928e39b06c413de07e00525
Ivand14 <carpivan14@hotmail.com>
2025-06-24 10:17:06 -0300

    Update SocketIO run configuration to use environment-defined port

--- app.py
+++ app.py
@@ -93,7 +93,8 @@ def home():
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=int(10000))
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
 
 
 

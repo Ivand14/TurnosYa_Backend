00192839eb8310fffbcfc1c473f4b83db6ea1519
Ivand14 <carpivan14@hotmail.com>
2025-07-14 20:50:28 -0300

    add block try except in all_employees

--- routes/company/all_employees.py
+++ routes/company/all_employees.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify
 from config import firebase_service
 
 ALL_EMPLOYEE = Blueprint("ALL_EMPLOYEE", __name__)
@@ -6,17 +6,25 @@ ALL_EMPLOYEE = Blueprint("ALL_EMPLOYEE", __name__)
 @ALL_EMPLOYEE.route("/all_employee/<businessId>", methods=["GET"])
 def get_employees(businessId):
     db = firebase_service.db
-    employees_ref = db.collection("employees").where("businessId","==",businessId)
-    employees_doc = employees_ref.stream()
+    try:
+        employees_ref = db.collection("employees").where("businessId","==",businessId)
+        employees_doc = employees_ref.stream()
+        employees = [{"id": doc.id, **doc.to_dict()} for doc in employees_doc]
     
-    employees = [{"id": doc.id, **doc.to_dict()} for doc in employees_doc]
-    
-    if not employees:
+        return jsonify({
+                "status": 201,  
+                "details": employees,
+            })
+    except Exception as e:
         return jsonify({
             "status": 404,  
             "details": "Error al encontrar los empleados",
         })
-    return jsonify({
-            "status": 201,  
-            "details": employees,
-        })
+    
+    
+    
+    
+    
+    
+    
+    

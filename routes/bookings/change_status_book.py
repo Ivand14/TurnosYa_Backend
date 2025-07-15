5d14227f5980720c1ffcac58755166ca5a5ed97c
Ivand14 <carpivan14@hotmail.com>
2025-07-15 11:12:13 -0300

    fix attemp errors

--- routes/bookings/change_status_book.py
+++ routes/bookings/change_status_book.py
@@ -7,7 +7,7 @@ STATUS_BOOK = Blueprint("STATUS_BOOK", __name__)
 @STATUS_BOOK.route("/status_book", methods=["PATCH"])
 def status_book():
     db = firebase_service.db
-    data = request.json
+    data = request.get_json()
 
     if not data or "booking_id" not in data or "new_status" not in data:
         return jsonify({"status": 400, "details": "Faltan datos requeridos"}), 400
@@ -16,20 +16,28 @@ def status_book():
     new_status = data["new_status"]
 
     try:
-        booking_ref = db.collection("reservas").where("id","==",booking_id).get()
+        booking_ref = db.collection("reservas").where("id", "==", booking_id).get()
         booking_list = list(booking_ref)
 
-        if not booking_ref.exists:
+        if not booking_list:
             return jsonify({"status": 404, "details": "Reserva no encontrada"}), 404
 
         for book in booking_list:
             book.reference.update({"status": new_status})
 
-        update_status = {"id": booking_ref.id, **booking_ref.to_dict()}
-        socketio.emit("update_status_book", update_status)
+        update_status = [
+            {"id": book.id, **book.to_dict()} for book in booking_list
+        ]
+
+        socketio.emit("update_status_book", {
+            "action": "update",
+            "reservas": update_status
+        })
 
         return jsonify({"status": 200, "details": update_status}), 200
 
     except Exception as e:
-        return jsonify({"status": 500, "details": f"Error interno: {e}"}), 500
-    
+        return jsonify({
+            "status": 500,
+            "details": f"Error interno: {e}"
+        }), 500
